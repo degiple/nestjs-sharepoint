4646f8a68b6db334dc08ade4530075382598b506
Konosuke Takeshima <k.takeshima@degiple.com>
2023-08-06 11:45:57 +0900

    add search

--- src/sharepoint/sharepoint.controller.ts
+++ src/sharepoint/sharepoint.controller.ts
@@ -1,5 +1,4 @@
-// sharepoint.controller.ts
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { SharePointService } from './sharepoint.service';
 
 @Controller('sharepoint')
@@ -7,7 +6,17 @@ export class SharePointController {
   constructor(private readonly sharePointService: SharePointService) {}
 
   @Get()
-  async getAllSites(): Promise<any> {
-    return this.sharePointService.getAllSites();
+  async search(
+    @Query('title') title: string,
+    @Query('author') author: string,
+    @Query('contenttype') contenttype: string,
+    @Query('operator') operator = 'AND',
+  ) {
+    let query = '';
+    if (title) query += `title:${title} `;
+    if (author) query += `${operator} author:${author} `;
+    if (contenttype) query += `${operator} contenttype:${contenttype}`;
+
+    return await this.sharePointService.search(query.trim());
   }
 }
--- src/sharepoint/sharepoint.service.ts
+++ src/sharepoint/sharepoint.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { Client } from '@microsoft/microsoft-graph-client';
 import * as msRestNodeAuth from '@azure/ms-rest-nodeauth';
+import { Client } from '@microsoft/microsoft-graph-client';
+import axios from 'axios';
 
 @Injectable()
 export class SharePointService {
+  private accessToken: string;
   private client: Client;
 
   constructor() {
@@ -21,9 +23,11 @@ export class SharePointService {
     );
 
     const tokenResponse = await credentials.getToken();
+    this.accessToken = tokenResponse.accessToken;
+
     this.client = Client.init({
       authProvider: (done) => {
-        done(null, tokenResponse.accessToken);
+        done(null, this.accessToken);
       },
     });
   }
@@ -36,5 +40,18 @@ export class SharePointService {
   async getSearchSite(siteName: string): Promise<any> {
     const site = await this.client.api(`/sites/${siteName}`).get();
     return site;
+  }
+
+  async search(query: string): Promise<any> {
+    const response = await axios.get(
+      `https://graph.microsoft.com/v1.0/sites?search=${query}`,
+      {
+        headers: {
+          Authorization: `Bearer ${this.accessToken}`,
+        },
+      },
+    );
+
+    return response.data;
   }
 }
